a050c47860a9253fde6588520abd5bb3996d12aa
masatotezuka <tez.0731.tez@gmail.com>
2022-07-10 17:36:32 +0900

    cookie-parser・cors・パッケージの型定義をインストール

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
 import { AppDataSource } from "../data-source";
-
+import * as cors from "cors";
+import * as dotenv from "dotenv";
+import * as cookieParser from "cookie-parser";
+import * as express from "express";
 AppDataSource.initialize();
