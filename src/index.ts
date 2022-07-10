dfbc3bbf5949994cec8c1ece7fdf3e54694f6744
masatotezuka <tez.0731.tez@gmail.com>
2022-07-10 17:42:59 +0900

    サーバーの立ち上げ・ミドルウェアの設定

--- src/index.ts
+++ src/index.ts
@@ -3,4 +3,25 @@ import * as cors from "cors";
 import * as dotenv from "dotenv";
 import * as cookieParser from "cookie-parser";
 import * as express from "express";
-AppDataSource.initialize();
+
+const app = express();
+
+const corsOptions: cors.CorsOptions = {
+  origin: "http://localhost:3000",
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cookieParser());
+
+AppDataSource.initialize()
+  .then(() => {
+    console.log("Data Source has been initialized!");
+  })
+  .catch((err) => {
+    console.error("Error during Data Source initialization:", err);
+  });
+
+app.listen(8000);
