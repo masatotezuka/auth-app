784f80c9d4f25e4a37af13523e73bfab66178a23
masatotezuka <tez.0731.tez@gmail.com>
2022-07-11 10:31:13 +0900

    jwtトークン の設定/msのインストール

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,12 +1,46 @@
 import * as express from "express";
+import * as jwt from "jsonwebtoken";
+import ms = require("ms");
 import login from "./login";
 import signUp from "./signUp";
 import logout from "./logout";
 import home from "./home";
+
 const router = express.Router();
 
+//認証前ページからのデータ処理のルーティング
 router.use("/sign-up", signUp);
 router.use("/login", login);
 router.use("logout", logout);
 
+//jwtトークンの検証
+router.get("/tokenVerification", (req, res, next) => {
+  let token = "";
+  if (req.cookies.jwtToken) {
+    token = req.cookies.jwtToken;
+  } else {
+    //cookieにjwtトークンがない場合は、認証不可
+    return res.status(200).json({ isAuthenticated: false });
+  }
+
+  //  リクエストされたjwtトークンを検証
+  jwt.verify(token, "secret123", function (err, decoded) {
+    if (err) {
+      console.log(err);
+    } else {
+      //検証がOKであれば、jwtトークンを再作成
+      const jweSecret = "secret123";
+      const token = jwt.sign({ foo: "bar" }, jweSecret, {
+        expiresIn: "30d",
+      });
+      res.cookie("jwtToken", token, {
+        httpOnly: true,
+        expires: new Date(Date.now() + ms("2d")),
+      });
+      res.status(200).json({ isAuthenticated: true });
+    }
+  });
+});
+
+//認証後ページからのデータ処理のルーティング
 router.use("/home", home);
