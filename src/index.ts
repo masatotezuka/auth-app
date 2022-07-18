6c6883d0b4c6408559b1d496b2dd22f5dc894a5a
masatotezuka <tez.0731.tez@gmail.com>
2022-07-18 11:22:06 +0900

    トップファイル修正

--- src/index.ts
+++ src/index.ts
@@ -5,17 +5,22 @@ import * as cookieParser from "cookie-parser";
 import * as express from "express";
 
 dotenv.config();
-
 const app = express();
 
 const corsOptions: cors.CorsOptions = {
+  //フロントエンド側のポート番号を設定する
   origin: "http://localhost:3000",
-  credentials: true,
+  //認証情報の通信をするためcredentialsはtrueにする
+  credentials: false,
 };
 
+//3000番ポートのリクエストを許可
 app.use(cors(corsOptions));
+//URLのなかでエンコードされた文字を読み取れるようにする
 app.use(express.urlencoded({ extended: true }));
+//リクエストされたJSONオブジェクトを読み取れるようにする
 app.use(express.json());
+//リクエストされたcookieを読み取れるようにする
 app.use(cookieParser());
 
 AppDataSource.initialize()
