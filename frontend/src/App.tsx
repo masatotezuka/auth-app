fc2cf172ef88d5402e3e9ba876c104ed2030b4a7
masatotezuka <tez.0731.tez@gmail.com>
2022-07-21 16:20:13 +0900

    ルーティング

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,19 @@
 import React from "react";
-function App() {
-  return <div className="App"></div>;
-}
+import { Routes, Route } from "react-router-dom";
+import { TopPage } from "./components/pages/TopPage";
+import { HomePage } from "./components/pages/HomePage";
+import { SignUpPage } from "./components/pages/SignUpPage";
+import { LoginPage } from "./components/pages/LoginPage";
 
-export default App;
+export const App = () => {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<TopPage />} />
+        <Route path="/sign-up" element={<SignUpPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/home" element={<HomePage />} />
+      </Routes>
+    </>
+  );
+};
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,16 +1,15 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import { Provider } from "react-redux";
-// import { store } from "./app/store";
-import App from "./App";
+import { BrowserRouter } from "react-router-dom";
+import { App } from "./App";
 
 const container = document.getElementById("root")!;
 const root = createRoot(container);
 
 root.render(
   <React.StrictMode>
-    {/* <Provider store={store}> */}
-    <App />
-    {/* </Provider> */}
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
