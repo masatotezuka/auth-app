4cb3edfeb6425220e90681ad026921fac25dc62f
masatotezuka <tez.0731.tez@gmail.com>
2022-07-21 15:50:33 +0900

    topページ作成

--- frontend/src/component/pages/TopPage/index.tsx
+++ frontend/src/component/pages/TopPage/index.tsx
@@ -1,5 +1,11 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export const TopPage = () => {
-  return <div>TopPage</div>;
+  return (
+    <div>
+      <Link to={"/sign-up"}>登録</Link>
+      <Link to={"/login"}>ログイン</Link>
+    </div>
+  );
 };
