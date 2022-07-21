0a43f6c4fa144475a9add9ee36be2ab4b1441dff
masatotezuka <tez.0731.tez@gmail.com>
2022-07-21 15:42:00 +0900

    余分なコードとファイルを削除

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
-import { store } from "./app/store";
+// import { store } from "./app/store";
 import App from "./App";
 
 const container = document.getElementById("root")!;
@@ -9,8 +9,8 @@ const root = createRoot(container);
 
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    {/* <Provider store={store}> */}
+    <App />
+    {/* </Provider> */}
   </React.StrictMode>
 );
