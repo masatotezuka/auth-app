5718186365d629a64b3d7626d94b39811ffba03e
masatotezuka <tez.0731.tez@gmail.com>
2022-07-22 22:42:59 +0900

    新規登録ページ作成

--- frontend/src/components/pages/SignUpPage/index.tsx
+++ frontend/src/components/pages/SignUpPage/index.tsx
@@ -4,14 +4,14 @@ export const SignUpPage = () => {
   return (
     <div>
       <form>
-        <label htmlFor="email">性</label>
-        <input id="email" type="text" name="firstName" />
+        <label htmlFor="firstName">性</label>
+        <input id="firstName" type="text" name="firstName" />
         <label htmlFor="lastName">名</label>
         <input id="lastName" type="text" name="lastName" />
-        <label htmlFor="email">Eメール</label>
-        <input id="email" type="email" name="email" />
-        <label htmlFor="password">パスワード</label>
-        <input id="password" type="password" name="password" />
+        <label htmlFor="email_register">Eメール</label>
+        <input id="email_register" type="email" name="email" />
+        <label htmlFor="password_register">パスワード</label>
+        <input id="password_register" type="password" name="password" />
         <button type="submit">新規登録</button>
       </form>
     </div>
