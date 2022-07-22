ce4e7ec1bdf46d91299c4488ced6607a4d336b24
masatotezuka <tez.0731.tez@gmail.com>
2022-07-22 22:36:26 +0900

    ログインページ作成

--- frontend/src/components/pages/LoginPage/index.tsx
+++ frontend/src/components/pages/LoginPage/index.tsx
@@ -1,5 +1,15 @@
 import React from "react";
 
 export const LoginPage = () => {
-  return <div>LoginPage</div>;
+  return (
+    <div>
+      <form>
+        <label htmlFor="email">Eメール</label>
+        <input id="email" type="email" name="email" />
+        <label htmlFor="password">パスワード</label>
+        <input id="password" type="password" name="password" />
+        <button type="submit">ログイン</button>
+      </form>
+    </div>
+  );
 };
