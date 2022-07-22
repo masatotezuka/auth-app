767192bf8049d065cb01452f9f8dcb91ec0d3aff
masatotezuka <tez.0731.tez@gmail.com>
2022-07-22 22:44:08 +0900

    modify login page

--- frontend/src/components/pages/LoginPage/index.tsx
+++ frontend/src/components/pages/LoginPage/index.tsx
@@ -4,10 +4,10 @@ export const LoginPage = () => {
   return (
     <div>
       <form>
-        <label htmlFor="email">Eメール</label>
-        <input id="email" type="email" name="email" />
-        <label htmlFor="password">パスワード</label>
-        <input id="password" type="password" name="password" />
+        <label htmlFor="email_login">Eメール</label>
+        <input id="email_login" type="email" name="email" />
+        <label htmlFor="password_login">パスワード</label>
+        <input id="password_login" type="password" name="password" />
         <button type="submit">ログイン</button>
       </form>
     </div>
