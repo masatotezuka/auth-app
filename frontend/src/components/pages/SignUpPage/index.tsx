e8e42234978dc48f530cdab5b034bec6c81ec63a
masatotezuka <tez.0731.tez@gmail.com>
2022-07-23 17:41:18 +0900

    add frontend signup

--- frontend/src/components/pages/SignUpPage/index.tsx
+++ frontend/src/components/pages/SignUpPage/index.tsx
@@ -1,17 +1,50 @@
 import React from "react";
+import { useForm, SubmitHandler } from "react-hook-form";
+import { signUp, User } from "../../../api/index";
 
 export const SignUpPage = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<User>();
+
+  const onSubmit: SubmitHandler<User> = async (data) => {
+    await signUp(data);
+  };
   return (
     <div>
-      <form>
-        <label htmlFor="firstName">性</label>
-        <input id="firstName" type="text" name="firstName" />
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <label htmlFor="firstName">
+          性
+          <input
+            id="firstName"
+            type="text"
+            {...register("firstName", { required: true })}
+          />
+        </label>
+        <p> {errors.firstName && "文字が入力されていません"}</p>
         <label htmlFor="lastName">名</label>
-        <input id="lastName" type="text" name="lastName" />
+        <input
+          id="lastName"
+          type="text"
+          {...register("lastName", { required: true })}
+        />
+        <p> {errors.lastName && "文字が入力されていません"}</p>
         <label htmlFor="email_register">Eメール</label>
-        <input id="email_register" type="email" name="email" />
+        <input
+          id="email_register"
+          type="email"
+          {...register("mail", { required: true })}
+        />
+        <p> {errors.mail && "文字が入力されていません"}</p>
         <label htmlFor="password_register">パスワード</label>
-        <input id="password_register" type="password" name="password" />
+        <input
+          id="password_register"
+          type="password"
+          {...register("password", { required: true })}
+        />
+        <p> {errors.password && "文字が入力されていません"}</p>
         <button type="submit">新規登録</button>
       </form>
     </div>
