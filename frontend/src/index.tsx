b821abae3fb29a6d2bc365398e73a3d6211300ee
masatotezuka <tez.0731.tez@gmail.com>
2022-07-23 17:09:44 +0900

    update top root

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,9 +1,7 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 import { App } from "./App";
-import { store } from "./store";
 
 const container = document.getElementById("root")!;
 const root = createRoot(container);
@@ -11,9 +9,7 @@ const root = createRoot(container);
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <Provider store={store}>
-        <App />
-      </Provider>
+      <App />
     </BrowserRouter>
   </React.StrictMode>
 );
