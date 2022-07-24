e68f6f05a6848883aa99331006c73beb4939e46b
masatotezuka <tez.0731.tez@gmail.com>
2022-07-24 12:07:52 +0900

    add auth router

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,15 +5,28 @@ import { HomePage } from "./components/pages/HomePage";
 import { SignUpPage } from "./components/pages/SignUpPage";
 import { LoginPage } from "./components/pages/LoginPage";
 import { NotFoundPage } from "./components/pages/NotFoundPage";
+import { GuestRoute, PrivateRoute } from "./AuthRouter";
 
 export const App = () => {
   return (
     <>
       <Routes>
-        <Route path="/" element={<TopPage />} />
-        <Route path="/sign-up" element={<SignUpPage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/home" element={<HomePage />} />
+        <Route
+          path="/"
+          element={<GuestRoute children={<TopPage />}></GuestRoute>}
+        />
+        <Route
+          path="/sign-up"
+          element={<GuestRoute children={<SignUpPage />}></GuestRoute>}
+        />
+        <Route
+          path="/login"
+          element={<GuestRoute children={<LoginPage />}></GuestRoute>}
+        />
+        <Route
+          path="/home"
+          element={<PrivateRoute children={<HomePage />}></PrivateRoute>}
+        />
         <Route path="*" element={<NotFoundPage />} />
       </Routes>
     </>
---
+++ frontend/src/AuthRouter.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { Navigate } from "react-router-dom";
+import { useAuth } from "./hooks/useAuth";
+
+type Props = {
+  children: React.ReactNode;
+};
+
+export const PrivateRoute = ({ children }: Props) => {
+  const check = useAuth();
+
+  if (!check.checked) {
+    return <div>Loading...</div>;
+  }
+  if (check.isAuthenticated) {
+    return <>{children}</>;
+  }
+
+  return <Navigate to="/" />;
+};
+
+export const GuestRoute = (props: Props) => {
+  const { children } = props;
+  const check = useAuth();
+  if (!check.checked) {
+    return <div>Loading...</div>;
+  }
+
+  if (check.isAuthenticated) {
+    return <Navigate to="/home" />;
+  }
+
+  return <>{children}</>;
+};
